2daf5236460570be4a7a3c8e086c936573e502a3
Chrys <bagwellchristopher@hotmail.co.uk>
2026-02-25 08:49:37 +0000

    tmux: Machine-dependent status

---
+++ tmux/status.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ -z "$CHS_MACHINE" ]]; then
+  echo "Duck: 𐦖 | Pablo's cock: ꜥ==ᴈ"
+else
+  battery="$(~/battery_percentage.sh -c)"
+  echo "Pablo's cock: $battery"
+fi
+

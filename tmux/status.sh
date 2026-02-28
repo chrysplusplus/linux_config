14e219fd747bd85d4a8a91bda6cf8c6145a77b4e
Chrys <bagwellchristopher@hotmail.co.uk>
2026-02-28 17:41:20 +0000

    tmux: Added sfw toggle

--- tmux/status.sh
+++ tmux/status.sh
@@ -2,6 +2,9 @@
 
 if [[ -z "$CHS_MACHINE" ]]; then
   echo "Duck: 𐦖 | Pablo's cock: ꜥ==ᴈ"
+elif [[ -e "$HOME/.safe_tmux_status" ]]; then
+  battery="$(~/battery_percentage.sh)"
+  echo "Battery: $battery"
 else
   battery="$(~/battery_percentage.sh -c)"
   echo "Pablo's cock: $battery"

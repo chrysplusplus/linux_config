1a35322c17883e56415a858f4d03b7544298e1a8
Chrys <bagwellchristopher@hotmail.co.uk>
2026-08-16 01:06:59 +0100

    tmux: Improved status styling

--- tmux/status.sh
+++ tmux/status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$CHS_MACHINE" ]]; then
-  echo "Duck: 𐦖 | Pablo's cock: ꜥ===ᴈ"
+  echo "Duck: 𐦖 #[fg=#505762]|#[fg=white] Pablo's cock: ꜥ===ᴈ"
 elif [[ -e "$HOME/.safe_tmux_status" ]]; then
   battery="$(~/battery_percentage.sh)"
   echo "Battery: $battery"
